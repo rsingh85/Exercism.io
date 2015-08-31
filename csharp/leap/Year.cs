a6578e97144d15d973b7b11b0760c087702a7799
Ravinder Singh <rsingh@ravi-singh.com>
2015-08-31 22:13:07 +0100

    Initial commit.

---
+++ csharp/leap/Year.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercism.leap
+{
+    public static class Year
+    {
+        public static bool IsLeap(int year)
+        {
+            return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        }
+    }
+}
