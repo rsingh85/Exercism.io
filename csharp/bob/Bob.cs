187a6f426a6f5b137578a142da3321af3dc224f4
Douglas Parsons <douglas.parsons@civil.lmco.com>
2015-09-07 11:02:44 +0100

    Tidied up bob

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -6,30 +6,16 @@ public static class Bob
     {
         input = input.Trim();
 
-        if (input.IsShouting())
-            return "Whoa, chill out!";
-
-        if (input.IsQuestion())
-            return "Sure.";
-        
         if (input == string.Empty)
             return "Fine. Be that way!";
 
-        return "Whatever.";
-    }
-}
+        if (!input.Any(c => char.IsLower(c)) && input.Any(c => char.IsLetter(c)))
+            return "Whoa, chill out!";
 
-public static class BobStringExtensions
-{
-    public static bool IsShouting(this string @string)
-    {
-        return @string.Any(c => char.IsLetter(c)) &&
-                @string.Where(c => char.IsLetter(c))
-                .All(c => char.IsUpper(c));
-    }
-
-    public static bool IsQuestion(this string @string)
-    {
-        return @string.EndsWith("?");
+        if (input.EndsWith("?"))
+            return "Sure.";
+        
+        else
+            return "Whatever.";
     }
 }
