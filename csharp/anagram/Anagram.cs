4edf28d856a191f149a075f55571b6f881ae608a
Ravinder Singh <rsingh@ravi-singh.com>
2015-09-14 00:48:32 +0100

    Fetched Anagram exercise and stubbed out Anagram class.

---
+++ csharp/anagram/Anagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Anagram
+{
+    private readonly string input;
+
+    public Anagram(string input)
+    {
+        this.input = input;
+    }
+
+    public string[] Match(string[] words)
+    {
+        throw new NotImplementedException();
+    }
+}
